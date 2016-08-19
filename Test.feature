800dd4f5aedf10dd088a5eda3a2ff91209400fb0
Robert Glas <robert.glas@vodafone.com>
2016-08-19 12:10:24 +0200

    switch to regex step matcher

--- Test.feature
+++ Test.feature
@@ -3,11 +3,11 @@
 Feature: Behave Selenium Showcase
 
 Scenario: Open Google and verify the page title is correct
-    Given I open google.com
+    Given I open "google.com"
     Then the title should contain "Google"
 
 @fails
 Scenario: Open wrong page and let the test fail
-    Given I open google-not-found.com
+    Given I open "google-not-found.com"
     Then the title should contain "Google"
 
--- steps/steps.py
+++ steps/steps.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
+from behave import *
+use_step_matcher("re")
 
 @given(u'I open (?P<url>.+)')
 def step_impl(context, url):
     context.driver.get("http://www.%s/" % url)
 
-
-@step(u'the title should contain "{title}"')
+@step(u'the title should contain "(?P<title>.+)"')
 def step_impl(context, title):
     browser_title = context.driver.title
     assert title in browser_title, 'Found "%s" instead ' % browser_title
